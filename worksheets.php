cfb3df48c27e5ab84f6aa72a000a04726a6f55d3
Rohul Amin Mohammad Dipu <41869272+radipu@users.noreply.github.com>
2022-01-16 20:11:04 +0600

    completed task management system

---
+++ worksheets.php
@@ -0,0 +1,120 @@
+<?php include'db_connect.php' ?>
+<div class="col-lg-12">
+	<div class="card card-outline card-success">
+		<div class="card-header">
+			<div class="card-tools">
+				<a class="btn btn-block btn-sm btn-default btn-flat border-primary" href="./index.php?page=new_project"><i class="fa fa-plus"></i> Add New project</a>
+			</div>
+		</div>
+		<div class="card-body">
+			<table class="table tabe-hover table-bordered" id="list">
+				<colgroup>
+					<col width="5%">
+					<col width="15%">
+					<col width="20%">
+					<col width="15%">
+					<col width="15%">
+					<col width="10%">
+					<col width="10%">
+					<col width="10%">
+				</colgroup>
+				<thead>
+					<tr>
+						<th class="text-center">#</th>
+						<th class="text-center">Project</th>
+						<th class="text-center">Task</th>
+						<th class="text-center">From Date</th>
+						<th class="text-center">To Date</th>
+						<th class="text-center">Time</th>
+						<th class="text-center">Note</th>
+						<th class="text-center">User</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					$i = 1;
+					$where = "";
+					if($_SESSION['login_type'] == 2){
+						$where = " where p.manager_id = '{$_SESSION['login_id']}' ";
+					}elseif($_SESSION['login_type'] == 3){
+						$where = " where concat('[',REPLACE(p.user_ids,',','],['),']') LIKE '%[{$_SESSION['login_id']}]%' ";
+					}
+					
+					$stat = array("Pending","Started","Running","On-Hold","Over Due","Pause");
+					$qry = $conn->query("SELECT t.*,p.name as pname,p.start_date,p.status as pstatus, p.end_date,p.id as pid FROM task_list t inner join project_list p on p.id = t.project_id $where order by p.name asc");
+					while($row= $qry->fetch_assoc()):
+						$trans = get_html_translation_table(HTML_ENTITIES,ENT_QUOTES);
+						unset($trans["\""], $trans["<"], $trans[">"], $trans["<h2"]);
+						$desc = strtr(html_entity_decode($row['description']),$trans);
+						$desc=str_replace(array("<li>","</li>"), array("",", "), $desc);
+						$tprog = $conn->query("SELECT * FROM task_list where project_id = {$row['pid']}")->num_rows;
+		                $cprog = $conn->query("SELECT * FROM task_list where project_id = {$row['pid']} and status = 3")->num_rows;
+						$prog = $tprog > 0 ? ($cprog/$tprog) * 100 : 0;
+		                $prog = $prog > 0 ?  number_format($prog,2) : $prog;
+		                $prod = $conn->query("SELECT * FROM user_productivity where project_id = {$row['pid']}")->num_rows;
+						$dur = $conn->query("SELECT sum(time_rendered) as duration FROM user_productivity where project_id = {$row['id']}");
+                		$dur = $dur->num_rows > 0 ? $dur->fetch_assoc()['duration'] : 0;
+		                if($row['pstatus'] == 0 && strtotime(date('Y-m-d')) >= strtotime($row['start_date'])):
+		                if($prod  > 0  || $cprog > 0)
+		                  $row['pstatus'] = 2;
+		                else
+		                  $row['pstatus'] = 1;
+		                elseif($row['pstatus'] == 0 && strtotime(date('Y-m-d')) > strtotime($row['end_date'])):
+		                $row['pstatus'] = 4;
+		                endif;
+					?>
+					<tr>
+						<td class="text-center"><?php echo $i++ ?></td>
+						<td>
+							<p><b><?php echo ucwords($row['pname']) ?></b></p>
+						</td>
+						<td>							
+							<p class="truncate"><?php echo strip_tags($desc) ?></p>
+						</td>
+						<td><b><?php echo date("M d, Y",strtotime($row['start_date'])) ?></b></td>
+						<td><b><?php echo date("M d, Y",strtotime($row['end_date'])) ?></b></td>
+            			<td class="text-center">
+                      		<?php echo number_format($dur).' Hr/s.' ?>
+                      	</td>
+                      	<td><b><?php echo $row['note'] ?></b></td>
+						<td><b></b></td>
+					</tr>	
+				<?php endwhile; ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+<style>
+	table p{
+		margin: unset !important;
+	}
+	table td{
+		vertical-align: middle !important
+	}
+</style>
+<script>
+	$(document).ready(function(){
+		$('#list').dataTable()
+	$('.new_productivity').click(function(){
+		uni_modal("<i class='fa fa-plus'></i> New Progress for: "+$(this).attr('data-task'),"manage_progress.php?pid="+$(this).attr('data-pid')+"&tid="+$(this).attr('data-tid'),'large')
+	})
+	})
+	function delete_project($id){
+		start_load()
+		$.ajax({
+			url:'ajax.php?action=delete_project',
+			method:'POST',
+			data:{id:$id},
+			success:function(resp){
+				if(resp==1){
+					alert_toast("Data successfully deleted",'success')
+					setTimeout(function(){
+						location.reload()
+					},1500)
+
+				}
+			}
+		})
+	}
+</script>
